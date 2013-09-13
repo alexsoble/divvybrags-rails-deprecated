e7dac4b1ce2be67a3dbb52fef9085e4ebeaa6fe2
alexsoble <asoble@gmail.com>
2013-09-12 21:38:17 -0500

    Commenting out the export-to-Google code in the Posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,10 @@ class PostsController < ApplicationController
   require 'mechanize'
   require 'rubygems'
   require 'csv'
-  # /Users/alexsoble/.rvm/gems/ruby-1.9.3-p362/gems/google-api-client-0.6.4/
   # require 'google/api-client'
-  require 'launchy'
+  # require 'launchy'
 
-  def talk_to_divvy(username, password, google_token)
+  def talk_to_divvy(username, password)
 
     agent = Mechanize.new
     page = agent.get('https://divvybikes.com/login')
@@ -21,12 +20,12 @@ class PostsController < ApplicationController
     rows = page.search("tr")
 
     # Build data table to export to Google Drive if user requested it. 
-    if google_token != "none" 
-      tmp_file = Tempfile.new("#{username}_divvytrips.csv")
-      CSV.open(tmp_file, "ab") do |csv|
-        csv << ["Start Station", "Start Date", "End Station", "End Date", "Duration"]
-      end
-    end
+    # if google_token != "none" 
+    #   tmp_file = Tempfile.new("#{username}_divvytrips.csv")
+    #   CSV.open(tmp_file, "ab") do |csv|
+    #     csv << ["Start Station", "Start Date", "End Station", "End Date", "Duration"]
+    #   end
+    # end
 
     result = []
     rows.each do |r|
@@ -41,89 +40,91 @@ class PostsController < ApplicationController
       data = "{ \"start_station\" : \"#{start_station}\", \"start_time\" : \"#{start_time}\", \"end_station\" : \"#{end_station}\", \"end_time\" : \"#{end_time}\", \"duration\" : \"#{duration}\" }"  
       result << data
 
-      if google_token != "none" 
-        unless start_station.blank? && end_station.blank?
-          CSV.open(tmp_file, "ab") do |csv|
-            csv << ["#{start_station}", "#{start_time}", "#{end_station}", "#{end_time}", "#{duration}"]
-          end
-        end
-      end
     end
 
-    if google_token != "none" 
-      # Create a new API client & load the Google Drive API 
-      client = Google::APIClient.new
-      drive = client.discovered_api('drive', 'v2')
+    #   if google_token != "none" 
+    #     unless start_station.blank? && end_station.blank?
+    #       CSV.open(tmp_file, "ab") do |csv|
+    #         csv << ["#{start_station}", "#{start_time}", "#{end_station}", "#{end_time}", "#{duration}"]
+    #       end
+    #     end
+    #   end
+    # end
 
-      # Request authorization
-      client.authorization.client_id = ENV["GOOGLE_KEY"]
-      client.authorization.client_secret = ENV["GOOGLE_SECRET"]
-      client.authorization.scope = 'https://www.googleapis.com/auth/drive'
-      client.authorization.redirect_uri = "http://www.divvybrags.com/#{username}"
+    # if google_token != "none" 
+    #   # Create a new API client & load the Google Drive API 
+    #   client = Google::APIClient.new
+    #   drive = client.discovered_api('drive', 'v2')
 
-      uri = client.authorization.authorization_uri
-      Launchy.open(uri)
+    #   # Request authorization
+    #   client.authorization.client_id = ENV["GOOGLE_KEY"]
+    #   client.authorization.client_secret = ENV["GOOGLE_SECRET"]
+    #   client.authorization.scope = 'https://www.googleapis.com/auth/drive'
+    #   client.authorization.redirect_uri = "http://www.divvybrags.com/#{username}"
 
-      # Exchange authorization code for access token
-      $stdout.write  "Enter authorization code: "
-      client.authorization.code = gets.chomp
-      client.authorization.fetch_access_token!
+    #   uri = client.authorization.authorization_uri
+    #   Launchy.open(uri)
 
-      # Insert a file
-      file = drive.files.insert.request_schema.new({
-        'title' => 'My document',
-        'description' => 'A test document',
-        'mimeType' => 'text/plain'
-      })
+    #   # Exchange authorization code for access token
+    #   $stdout.write  "Enter authorization code: "
+    #   client.authorization.code = gets.chomp
+    #   client.authorization.fetch_access_token!
 
-      media = Google::APIClient::UploadIO.new('document.txt', 'text/plain')
-      result = client.execute(
-        :api_method => drive.files.insert,
-        :body_object => file,
-        :media => media,
-        :parameters => {
-          'uploadType' => 'multipart',
-          'alt' => 'json'})
-    end
+    #   # Insert a file
+    #   file = drive.files.insert.request_schema.new({
+    #     'title' => 'My document',
+    #     'description' => 'A test document',
+    #     'mimeType' => 'text/plain'
+    #   })
+
+    #   media = Google::APIClient::UploadIO.new('document.txt', 'text/plain')
+    #   result = client.execute(
+    #     :api_method => drive.files.insert,
+    #     :body_object => file,
+    #     :media => media,
+    #     :parameters => {
+    #       'uploadType' => 'multipart',
+    #       'alt' => 'json'})
+    # end
     
     return(result.to_s)
 
   end
 
-  def insert_file(client, title, description, parent_id, mime_type, file_name)
-    drive = client.discovered_api('drive', 'v2')
-    file = drive.files.insert.request_schema.new({
-      'title' => title,
-      'description' => description,
-      'mimeType' => mime_type
-    })
-    # Set the parent folder.
-    if parent_id
-      file.parents = [{'id' => parent_id}]
-    end
-    media = Google::APIClient::UploadIO.new(file_name, mime_type)
-    result = client.execute(
-      :api_method => drive.files.insert,
-      :body_object => file,
-      :media => media,
-      :parameters => {
-        'uploadType' => 'multipart',
-        'alt' => 'json'})
-    if result.status == 200
-      return result.data
-    else
-      puts "An error occurred: #{result.data['error']['message']}"
-      return nil
-    end
-  end
+  # def insert_file(client, title, description, parent_id, mime_type, file_name)
+  #   drive = client.discovered_api('drive', 'v2')
+  #   file = drive.files.insert.request_schema.new({
+  #     'title' => title,
+  #     'description' => description,
+  #     'mimeType' => mime_type
+  #   })
+  #   # Set the parent folder.
+  #   if parent_id
+  #     file.parents = [{'id' => parent_id}]
+  #   end
+  #   media = Google::APIClient::UploadIO.new(file_name, mime_type)
+  #   result = client.execute(
+  #     :api_method => drive.files.insert,
+  #     :body_object => file,
+  #     :media => media,
+  #     :parameters => {
+  #       'uploadType' => 'multipart',
+  #       'alt' => 'json'})
+  #   if result.status == 200
+  #     return result.data
+  #   else
+  #     puts "An error occurred: #{result.data['error']['message']}"
+  #     return nil
+  #   end
+  # end
 
   def create
 
     @username = params[:username]
     @password = params[:password]
-    @google_token = params[:google_token]
+    # @google_token = params[:google_token]
 
-    divvy_data = talk_to_divvy(@username, @password, @google_token)
+    divvy_data = talk_to_divvy(@username, @password)
     
     @raw_trips = JSON.parse(divvy_data)
     @trips = []
