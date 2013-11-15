57054bb9e862afb270f2d8ebf66c29fd6af77ea9
alexsoble <asoble@gmail.com>
2013-11-15 14:32:43 -0600

    Opening Access-Control headers tochrome-extension://hjholnjdmkpijohoabkjdeffdpidlmei/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'chrome-extension://hjholnjdmkpijohoabkjdeffdpidlmei/'
+    headers['Access-Control-Request-Method'] = 'chrome-extension://hjholnjdmkpijohoabkjdeffdpidlmei/'
+  end
+
   helper_method :current_user
 
   private
---
+++ db/migrate/20131115155059_add_url_to_restaurants.rb
@@ -0,0 +1,4 @@
+class AddUrlToRestaurants < ActiveRecord::Migration
+  def change
+  end
+end
